fecbdac8080854d265217114967425a8066cc917
Victor Berbenets <berbenets.vitia@yandex.ru>
2023-11-21 19:26:23 +0300

    added switch in driver

--- frontend/parsing/include/driver.hpp
+++ frontend/parsing/include/driver.hpp
@@ -1,28 +1,38 @@
 #pragma once
 
+#include <string>
+
 #include "paracl_grammar.tab.hh"
 #include <FlexLexer.h>
 
 namespace yy {
 
 class Driver {
-  FlexLexer *plex_;
+    FlexLexer *plex_;
 
 public:
-  Driver(FlexLexer *plex) : plex_(plex) {}
+    Driver(FlexLexer *plex) : plex_(plex) {}
 
-  parser::token_type yylex(parser::semantic_type *yylval) {
-    parser::token_type tt = static_cast<parser::token_type>(plex_->yylex());
-    if (tt == yy::parser::token_type::NUMBER)
-      yylval->as<int>() = std::stoi(plex_->YYText());
-    return tt;
-  }
+    parser::token_type yylex(parser::semantic_type *yylval) {
+        parser::token_type tt = static_cast<parser::token_type>(plex_->yylex());
+        switch(tt) {
+          case yy::parser::token_type::NUMBER :
+              yylval->as<int>() = std::stoi(plex_->YYText());
+              break;
+          case yy::parser::token_type::VAR :
+              yylval->as<std::string>() = plex_->YYText();
+              break;
+          default: { /* do nothing */}
+        }
 
-  bool parse() {
-    parser parser(this);
-    bool res = parser.parse();
-    return !res;
-  }
+        return tt;
+    }
+
+    bool parse() {
+        parser parser(this);
+        bool res = parser.parse();
+        return !res;
+    }
 };
 
 } // namespace yy
