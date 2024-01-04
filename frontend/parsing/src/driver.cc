da9a85e749f86191ecfa91b108e0a21531ff372e
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-01-04 14:06:58 +0300

    improved incorrect input data handling

--- frontend/parsing/src/driver.cc
+++ frontend/parsing/src/driver.cc
@@ -2,17 +2,22 @@
 #include <stdexcept>
 #include <string>
 #include <sstream>
+#include <filesystem>
 #include <FlexLexer.h>
 
 #include "driver.hpp"
 #include "ast.hpp"
 #include "expression.hpp"
 
+namespace fs = std::filesystem;
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     std::cerr << "paraCL: fatal error: no input file" << std::endl <<
                  "Program terminated" << std::endl;
     return -1;
+  } else if (!fs::is_regular_file(argv[1])) {
+    std::cerr << "paraCL: error: can't oppen file: " << argv[1] << std::endl;
   }
 
   std::ifstream i_stream{argv[1]};
