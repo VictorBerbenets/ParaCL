167fbb802cad50259c301fde91f7eabf676284ca
Viktor Berbenets <v.berbenets@syntacore.com>
2025-02-16 13:35:12 +0300

    add command line opts

--- lib/parsing/paracl_compiler.cpp
+++ lib/parsing/paracl_compiler.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <llvm/Support/CommandLine.h>
 
-int main(int argc, char** argv) {
+#include <string>
+
+namespace cl = llvm::cl;
+
+cl::opt<std::string> InputFileName(cl::Positional, cl::desc("<input file>"),
+                                   cl::value_desc("filename"), cl::Required);
+
+cl::opt<std::string> OutputFileName("o", cl::desc("Specify output filename for llvm IR"),
+                                     cl::value_desc("filename"), cl::init("paracl.ll"), cl::Optional);
+
+
+
+int main(int argc, char** argv) try {
+  cl::ParseCommandLineOptions(argc, argv, " ParaCL (custom compile program)\n\n"
+                              " This program translates code from paraCL to llvm IR\n");
+
+} catch (...) {
 
 }
 
